0ef06538f40e7d408b7cfe6218765cb49f934a0e
magicmarkh <mark.t.hurter@gmail.com>
2025-05-07 13:54:05 -0500

    removed unnecessary output files

--- terraform_code/modules/infrastructure/ec2_instances/ec2_public_server/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "public_server_id" {
-  value = aws_instance.public_linux_server.id
-}
-
-output "public_server_ip" {
-  value = aws_instance.public_linux_server.public_ip
-}
--- terraform_code/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public_server_ip" {
-  value = module.ec2_public_server.public_server_ip
-}
